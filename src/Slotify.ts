a4577fcdd032f74ac8badfd828d4ae38799c6f11
Stanislau Herasimovich <pascaldelger@gmail.com>
2018-09-25 08:55:23 +0200

    Custom session class factory

--- src/Slotify.ts
+++ src/Slotify.ts
@@ -5,69 +5,71 @@ import {GameSessionParameters} from "./session/GameSessionParameters";
 import {IGameSessionModel} from "./session/IGameSessionModel";
 
 export class Slotify {
-
-    public static createReelGameSession(config?: IReelGameSessionConfig): IReelGameSession {
-        let sessionClass;
-
-        if (config && config.customSessionClass) {
-            sessionClass = ReelGameSession;
-        } else  {
-            sessionClass = class Session extends ReelGameSession {
-
-                constructor(){
-                    super();
-                }
-
-                protected initializeGlobalSessionParameters(): void {
-                    super.initializeGlobalSessionParameters();
-                    if (config) {
-                        if (config.availableBets) {
-                            GameSessionParameters.availableBets = config.availableBets;
-                        }
-                        if (config.paytable) {
-                            ReelGameSessionParameters.paytable = config.paytable;
-                        }
-                        if (config.availableItems) {
-                            ReelGameSessionParameters.availableItems = config.availableItems;
-                        }
-                        if (config.wildItemId) {
-                            ReelGameSessionParameters.wildItemId = config.wildItemId;
-                        }
-                        if (config.scatters) {
-                            ReelGameSessionParameters.scatters = config.scatters;
-                        }
-                        if (config.reelsNumber) {
-                            ReelGameSessionParameters.reelsNumber = config.reelsNumber;
-                        }
-                        if (config.reelsItemsNumber) {
-                            ReelGameSessionParameters.reelsItemsNumber = config.reelsItemsNumber;
-                        }
-                        if (config.reelsItemsSequences) {
-                            ReelGameSessionParameters.reelsItemsSequences = config.reelsItemsSequences;
-                        }
-                        if (config.linesDirections) {
-                            ReelGameSessionParameters.linesDirections = config.linesDirections;
-                        }
-                    }
-                }
     
-                protected createSessionModel(): IGameSessionModel {
-                    let r: IGameSessionModel;
-                    r = super.createSessionModel();
-                    if (config && config.creditsAmount !== undefined) {
-                        r.credits = config.creditsAmount;
+    public static createReelGameSession(config?: IReelGameSessionConfig): IReelGameSession {
+        let baseSessionClass;
+        let r;
+        if (config && config.customSessionClass) {
+            baseSessionClass = config.customSessionClass;
+        } else {
+            baseSessionClass = ReelGameSession;
+        }
+        r = new class Session extends baseSessionClass {
+            
+            constructor() {
+                super();
+            }
+            
+            protected initializeGlobalSessionParameters(): void {
+                super.initializeGlobalSessionParameters();
+                if (config) {
+                    if (config.availableBets) {
+                        GameSessionParameters.availableBets = config.availableBets;
                     }
-                    return r;
+                    if (config.paytable) {
+                        ReelGameSessionParameters.paytable = config.paytable;
+                    }
+                    if (config.availableItems) {
+                        ReelGameSessionParameters.availableItems = config.availableItems;
+                    }
+                    if (config.wildItemId) {
+                        ReelGameSessionParameters.wildItemId = config.wildItemId;
+                    }
+                    if (config.scatters) {
+                        ReelGameSessionParameters.scatters = config.scatters;
+                    }
+                    if (config.reelsNumber) {
+                        ReelGameSessionParameters.reelsNumber = config.reelsNumber;
+                    }
+                    if (config.reelsItemsNumber) {
+                        ReelGameSessionParameters.reelsItemsNumber = config.reelsItemsNumber;
+                    }
+                    if (config.reelsItemsSequences) {
+                        ReelGameSessionParameters.reelsItemsSequences = config.reelsItemsSequences;
+                    }
+                    if (config.linesDirections) {
+                        ReelGameSessionParameters.linesDirections = config.linesDirections;
+                    }
                 }
             }
-        }
-        return new sessionClass() as IReelGameSession;
+            
+            protected createSessionModel(): IGameSessionModel {
+                let r: IGameSessionModel;
+                r = super.createSessionModel();
+                if (config && config.creditsAmount !== undefined) {
+                    r.credits = config.creditsAmount;
+                }
+                return r;
+            }
+        };
+        
+        return r;
     }
-
+    
 }
 
 export interface IReelGameSessionConfig extends IGameSessionConfig {
-
+    
     paytable?: {
         [bet: number]: {
             [itemId: string]: {
@@ -75,29 +77,29 @@ export interface IReelGameSessionConfig extends IGameSessionConfig {
             }
         }
     };
-
+    
     availableItems?: string[];
-
+    
     wildItemId?: string;
-
+    
     scatters?: any[][];
-
+    
     reelsNumber?: number;
-
+    
     reelsItemsNumber?: number;
     
     reelsItemsSequences?: string[][];
-
+    
     linesDirections?: {};
-
+    
 }
 
 export interface IGameSessionConfig {
-
+    
     customSessionClass?: any;
-
+    
     availableBets?: number[];
     
     creditsAmount?: number;
-
+    
 }
