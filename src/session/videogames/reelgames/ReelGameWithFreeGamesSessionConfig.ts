9bbb6d56dc6584e90228ef3e098cc936fa367a97
Stanislau Herasimovich <pascaldelger@gmail.com>
2019-04-04 15:26:07 +0200

    Corrected ReelGameWithFreeGamesSessionConfig constructor parameters

--- src/session/videogames/reelgames/ReelGameWithFreeGamesSessionConfig.ts
+++ src/session/videogames/reelgames/ReelGameWithFreeGamesSessionConfig.ts
@@ -4,8 +4,8 @@ import {ReelGameSessionConfig} from "./ReelGameSessionConfig";
 export class ReelGameWithFreeGamesSessionConfig extends ReelGameSessionConfig implements IReelGameWithFreeGamesSessionConfig {
     private _freeGamesForScatters: { [p: string]: { [p: number]: number } };
 
-    constructor() {
-        super();
+    constructor(reelsNumber: number = 5, reelsItemsNumber: number = 3) {
+        super(reelsNumber, reelsItemsNumber);
         this._freeGamesForScatters = {
             "S": {
                 3: 10
